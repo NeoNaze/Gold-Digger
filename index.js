47a834e1f6e1eae0bbcad555259d9294d4687834
Herysia <totogirerd@gmail.com>
2017-11-05 16:17:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const Command = require('command')
 
 module.exports = function Drop(dispatch) {
-    const command = Command(dispatch)
-let old=-1,
-    amount=0,
-	copy
+	const command = Command(dispatch)
+	let old=-1,
+	amount=0
 
 command.add('money', amt => {
         amount = Number(amt)*10000 + amount
     })
 	
 dispatch.hook('S_INVEN', event=>{
-copy = event
-console.log(event.coins.low)
 	if(old===-1)
 	{
-		old=copy.coins.low
+		old=event.coins.low
 	}
-copy.coins.low=amount - (old-copy.coins.low)
-dispatch.toClient('S_INVEN', copy)
-return false
+	event.coins.low=amount - (old-event.coins.low)
+	return true
 })
+	
 }
